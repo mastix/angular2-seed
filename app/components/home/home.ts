a83c8db1617636fd4a27b4733cf30813c959c61d
Sascha Sambale <sascha.sambale@de.bosch.com>
2015-06-09 08:56:26 +0200

    first checkin with forms import

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,4 +1,5 @@
 import {Component, View} from 'angular2/angular2';
+import { FormBuilder, formDirectives, ControlGroup, Validators } from 'angular2/forms';
 
 @Component({
   selector: 'component-1'
